91c7b3c81fde600dbbf2cba1ea62a654400fddfe
Trung Nguyen <trungnt@hybrid-technologies.vn>
2023-08-15 21:40:27 +0700

    Initial Commit

---
+++ SwiftConcurrencyLearning/SwiftConcurrencyLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyLearningApp.swift
+//  SwiftConcurrencyLearning
+//
+//  Created by Trung Nguyen on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
